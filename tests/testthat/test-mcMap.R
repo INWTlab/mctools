ac63727587fced5953a04a5e3a43d3db112aeac3
Sebastian <wahani@gmail.com>
2018-02-07 20:32:37 +0100

    adapting test cases

--- tests/testthat/test-mcMap.R
+++ tests/testthat/test-mcMap.R
@@ -1,7 +1,6 @@
 testthat::context("MCMAP")
 
-VERBOSE <- FALSE
-MCMAP <- function(...) mctools::mcMap(..., verbose = VERBOSE)
+MCMAP <- function(..., errors = "suppress") mctools::mcMap(..., errors = errors)
 
 testthat::test_that("Errors", {
 
@@ -22,11 +21,11 @@ testthat::test_that("Errors", {
   )
 
   testthat::expect_equal(
-    MCMAP(3, testFun, errors = "stop")[[1]],
+    MCMAP(3, testFun, finallyStop = TRUE)[[1]],
     "all good"
   )
 
-  testthat::expect_error(MCMAP(1, testFun, errors = "stop"))
+  testthat::expect_error(MCMAP(1, testFun, finallyStop = TRUE))
 
 })
 
@@ -39,22 +38,21 @@ testthat::test_that("Warnings", {
     else "all good"
   }
 
+  options(warn = -1)
+
   testthat::expect_equal(MCMAP(1, testFun)[[1]], "some warning")
 
-  testthat::expect_error(MCMAP(1, testFun, warnings = "stop"))
+  testthat::expect_error(MCMAP(1, testFun, warnings = "asError", finallyStop = TRUE))
+
+  testthat::expect_equal(MCMAP(2, testFun)[[1]], "second")
 
   testthat::expect_equal(
-    lapply(attr(MCMAP(2, testFun, warnings = "return"), "warnings")[[1]], `[[`, "message"),
-    list("first", "second")
-  )
-
-  testthat::expect_equal(
-    MCMAP(3, testFun, warnings = "stop")[[1]],
+    MCMAP(3, testFun, warnings = "asError")[[1]],
     "nested suppress warning"
   )
 
   testthat::expect_equal(
-    MCMAP(4, testFun, warnings = "stop")[[1]],
+    MCMAP(4, testFun, warnings = "asError", finallyStop = TRUE)[[1]],
     "all good"
   )
 
@@ -67,17 +65,14 @@ testthat::test_that("Warnings and Errors", {
     else "all good"
   }
 
-  testthat::expect_is(
-    MCMAP(1, testFun)[[1]],
-    "error"
-  )
+  testthat::expect_is(MCMAP(1, testFun)[[1]], "error")
 
   testthat::expect_equal(
-    lapply(attr(MCMAP(1, testFun, warnings = "return"), "warnings")[[1]], `[[`, "message"),
-    list("first a warning")
+    MCMAP(1, testFun, warnings = "asError")[[1]]$message,
+    "Escalated warning: first a warning"
   )
 
-  testthat::expect_error(MCMAP(1, testFun, warnings = "stop"))
+  testthat::expect_error(MCMAP(1, testFun, warnings = "asError", finallyStop = TRUE))
 
 })
 
@@ -90,11 +85,12 @@ testthat::test_that("Whitelist for Warnings", {
   }
 
   testthat::expect_error(
-    MCMAP(1:3, testFun, warnings = "stop", warningsWhitelist = "^some warning$")
+    MCMAP(1:3, testFun, warnings = "asError", warningsWhitelist = "^some warning$",
+          finallyStop = TRUE)
   )
 
   testthat::expect_equal(
-    MCMAP(2, testFun, warnings = "stop", warningsWhitelist = "^some warning$")[[1]],
+    MCMAP(2, testFun, warnings = "asError", warningsWhitelist = "^some warning$")[[1]],
     "some warning"
   )
 
