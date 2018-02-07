1f07e445556e94fc30afb5da2c59dc3be112aa35
Sebastian <wahani@gmail.com>
2018-02-07 19:43:53 +0100

    update warnhandler

--- R/warnHandler.R
+++ R/warnHandler.R
@@ -1,4 +1,41 @@
-
 warnHandler <- function(warnings, ...) {
-  get(paste0("warn", warnings), mode = "function")(...)
+  stopifnot(warnings %in% c("log", "suppress", "asError"))
+  browser()
+  get(paste0("warn", tolower(warnings)), envir = environment(), mode = "function")(...)
+}
+
+warnlog <- function(...) {
+  function(w) {
+    futile.logger::flog.warn(w$message)
+    invokeRestart("muffleWarning")
+  }
+}
+
+warnsuppress <- function(...) {
+  function(w) {
+    invokeRestart("muffleWarning")
+  }
+}
+
+warnaserror <- function(whitelist, ...) {
+
+  force(whitelist)
+
+  onWhitelist <- function(msg) {
+    if (length(whitelist) == 0) FALSE
+    else any(unlist(lapply(whitelist, grepl, msg)))
+  }
+
+  function(w) {
+    if (onWhitelist(w$message)) {
+      futile.logger::flog.warn(w$message)
+      invokeRestart("muffleWarning")
+    }
+    else {
+      msg <- paste("Escalated warning:", w$message)
+      futile.logger::flog.error(msg)
+      simpleError(msg, w$call)
+    }
+  }
+
 }
